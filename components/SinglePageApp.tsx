cdd0b16967208b27676301d8665b1c0f872f7817
VidunderGunder <kristiankramas@outlook.com>
2022-10-16 19:29:19 +0200

    Done, I think

--- components/SinglePageApp.tsx
+++ components/SinglePageApp.tsx
@@ -13,9 +13,12 @@ export default forwardRef<HTMLDivElement, Props>(function SinglePageApp(
   // Your logic
   return (
     <Box ref={ref} {...props}>
-      {/* 
-        Your tags and components here.
-      */}
+      Your Single Page Application here. <br />
+      When you want to move to having multiple pages, you can look into how{" "}
+      <a href="https://nextjs.org/learn/foundations/about-nextjs?utm_source=next-site&utm_medium=homepage-cta&utm_campaign=next-website">
+        Next.js
+      </a>{" "}
+      works.
     </Box>
   );
 });
